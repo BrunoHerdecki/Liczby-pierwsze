b1716860547fff06aef6856fb60e6259a86e5ca5
Bruno Herdecki <Bruno.Herdecki@microsoft.wsei.edu.pl>
2021-10-21 00:19:07 +0200

    Dodaj pliki projektów.

---
+++ Liczby pierwsze/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Liczby_pierwsze
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("ile chcesz wypisac liczb pierwszych?");
+            int ile_wypisac = int.Parse(Console.ReadLine());
+            int ktora_liczba = 2;
+            Console.WriteLine($"{ile_wypisac} pierwszych licz pierwszych:");
+            while (ile_wypisac > 0)
+            {
+                bool jest_pierwsza = true;
+                for (int k = 2; k <= Math.Sqrt(ktora_liczba); k++)
+                {
+                    if (ktora_liczba % k == 0)
+                    {
+                        jest_pierwsza = false;
+                        break;
+                    }
+                }
+
+                if (jest_pierwsza)
+                {
+                    Console.WriteLine(ktora_liczba);
+                    ile_wypisac--;
+                }
+
+                ktora_liczba++;
+            }
+        }
+    }
+}
